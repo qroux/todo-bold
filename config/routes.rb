899094be581fd1b0181c284c0d5f2ce1a27e8ed8
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-09-10 18:10:41 +0200

    #create in Lists_controller + List  creation form in index.html with simple_form gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index] do
+  resources :lists, only: [:index, :new, :create] do
   end
 end
