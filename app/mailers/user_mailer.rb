e0ee725bdfc4dd1228d87234ecb40829f361c5d1
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-09-11 22:47:42 +0200

    daily reporting job working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,14 @@ class UserMailer < ApplicationMailer
     @user = User.find(params[:user])
     mail(to: @user.email, subject: 'Mission accomplie')
   end
+
+  def reporting
+    @admin = User.find_by(admin: true)
+
+    @users = User.count
+    @tasks_created = Task.where(done: false, created_at: 24.hours.ago..Time.now).count
+    @tasks_completed = Task.where(done: true, updated_at: 24.hours.ago..Time.now).count
+
+    mail(to: @admin.email, subject: 'Statistiques journalières')
+  end
 end
